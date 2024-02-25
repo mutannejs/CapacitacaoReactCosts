7d17afa80fcde082c0ea10dcc605da15d22ce596
mutannejs <murillojustinoklein@gmail.com>
2024-02-25 10:49:59 -0300

    footer e navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
 
@@ -13,23 +16,19 @@ function App() {
 
     <Router>
 
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <Navbar />
 
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
 
     </Router>
 
